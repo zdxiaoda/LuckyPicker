d36bd3e8b8438f8e482149a0f17c4861762ae260
zdxd <1638671689@qq.com>
2022-07-11 15:14:59 +0800

    删除设置字体~

--- src/js/path.js
+++ src/js/path.js
@@ -13,9 +13,8 @@ requirejs.config({
     vue: "./js/vue.min",
     jquery: "./js/jquery-3.6.0.min",
     name: "./js/name",
-    set_fonts: "./js/set-fonts",
     music: "./js/music",
   },
 
 });
-requirejs(["jquery", "screenfull", "music", "vue", "name", "set_fonts", "mdui"], function (dependency) { });
+requirejs(["jquery", "music", "vue", "name", "screenfull", "mdui"], function (dependency) { });
