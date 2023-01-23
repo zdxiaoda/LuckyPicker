ad6d669c6bea0b9f0763ad2b66b60f6a81e8e903
zdxiaoda <xiaoda@duck.com>
2023-01-23 23:36:16 +0800

    重写linux下的语音合成功能

--- src/js/music.js
+++ src/js/music.js
@@ -10,9 +10,11 @@ function music() {
 //判断为Windows还是Linux
 if (navigator.appVersion.indexOf("Win") != -1) {
   var listaddress = "/AppData/Roaming/random-roll-call-system";
+  var system = "Windows";
 }
 if (navigator.appVersion.indexOf("Linux") != -1) {
   var listaddress = "/.random-roll-call-system";
+  var system = "Linux";
 }
 function edittxt() {
   const { shell } = require("electron");
@@ -23,8 +25,32 @@ function speaker() {
     const utterance = new SpeechSynthesisUtterance(sentence);
     window.speechSynthesis.speak(utterance);
   }
-  //阅读文本
-  speak(document.getElementById("name").innerHTML, "utf-8");
+  //Windows系统 直接调用系统自带的语音合成
+  if (system == "Windows") {
+    speak(document.getElementById("name").innerHTML, "utf-8");
+  }
+  //Linux系统 调用espeak
+  if (system == "Linux") {
+    const { exec } = require("child_process");
+    exec(
+      "espeak -v zh " + document.getElementById("name").innerHTML,
+      (err, stdout) => {
+        if (err) {
+          //弹出错误信息
+          console.error(err);
+          var error_dialog = confirm(
+            "语音合成失败，请安装espeak\n点击“确定”将会打开终端，你可以使用当前系统拥有包管理器安装espeak"
+          );
+          if (error_dialog == true) {
+            //打开终端
+            const { exec } = require("child_process");
+            exec("gnome-terminal");
+          }
+          return;
+        }
+      }
+    );
+  }
 }
 //通过子窗口打开帮助/关于
 function open_help() {
