5dadba9e5eef0e20427862e2c519cdacd2a5718b
zdxiaoda <xiaoda@duck.com>
2023-02-24 21:58:45 +0800

    修改首次启动创造文本

--- src/js/name.js
+++ src/js/name.js
@@ -25,7 +25,7 @@ fs.access(file, fs.constants.F_OK, (the_list) => {
     //创建文件
     function createList() {
       const content = decodeURIComponent(
-        "%E4%BD%A0%E5%A5%BD%0A%E4%B8%96%E7%95%8C"
+        "%E4%BD%A0%E5%A5%BD%E4%B8%96%E7%95%8C%EF%BC%81"
       );
       fs.writeFile(
         require("os").homedir() + listaddress + "/list.txt",
