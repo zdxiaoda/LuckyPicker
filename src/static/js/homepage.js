e79df89e4d5f7bb8a9c8b080f4da4d2da7f2774b
zdxiaoda <xiaoda@duck.com>
2023-09-03 20:11:08 +0800

    移除更新代码

--- src/static/js/homepage.js
+++ src/static/js/homepage.js
@@ -1,22 +1,3 @@
-const localVersion = require("../package.json").version;
-const versionUrl =
-  "https://cdn.zhangda.xyz/random-roll-call-system/version.json";
-
-fetch(versionUrl)
-  .then((response) => response.json())
-  .then((cloudVersion) => {
-    if (localVersion < cloudVersion.latest_version) {
-      window.open(
-        "./subpage/show_update.html",
-        "_blank",
-        "width=521,height=325,frame=false,nodeIntegration=true,contextIsolation=false,enableRemoteModule=true"
-      );
-    }
-  })
-  .catch((error) => {
-    console.error(`Failed to fetch version information: ${error}`);
-  });
-
 //控制音乐播放
 function music() {
   const audio = document.getElementById("mokugyo");
